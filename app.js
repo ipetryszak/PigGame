7326106ffa1eea4768847c267e2cee5790dbffe5
ireneusz <ireneusz.petryszak@gmail.com>
2020-09-30 22:27:06 +0200

    added js

--- app.js
+++ app.js
@@ -8,3 +8,92 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+var scores, roundScore, activePlayer, stateVar;
+
+function resetVars() {
+  scores = [0,0];
+  roundScore = 0;
+  activePlayer = 0;
+  stateVar = 1;
+}
+function changeUser(){
+  activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+}
+function clearCurrentScore() {
+  document.querySelector('#current-0').textContent = '0';
+  document.querySelector('#current-1').textContent = '0';
+}
+function clearGlobalScore() {
+  document.querySelector('#score-0').textContent = '0';
+  document.querySelector('#score-1').textContent = '0';
+}
+function clearAndChange(){
+  changeUser();
+  clearCurrentScore();
+  roundScore = 0;
+  document.querySelector('.player-0-panel').classList.toggle('active');
+  document.querySelector('.player-1-panel').classList.toggle('active');
+  document.querySelector('.dice').style.display = 'none';
+}
+
+resetVars();
+//set dice pic unvisable
+document.querySelector('.dice').style.display = 'none';
+//set scores for 0
+clearGlobalScore();
+clearCurrentScore();
+
+document.querySelector('.btn-roll').addEventListener('click', () => {
+
+  if(stateVar){
+    //1. Random number
+    var diceNumber = Math.floor(Math.random() * 6) + 1;
+
+    //2. Show related picture of dice
+    var dicePicture = document.querySelector('.dice');
+    dicePicture.style.display = 'block';
+    dicePicture.src = 'dice-'+diceNumber+'.png';
+
+    //3. Update current score holder
+    if(diceNumber !== 1) {
+        roundScore += diceNumber;
+        document.querySelector('#current-'+activePlayer).textContent = roundScore;
+    }
+    else {
+        clearAndChange();
+    }
+  }
+})
+
+document.querySelector('.btn-hold').addEventListener('click', () => {
+
+    if(stateVar)
+    {
+      //1. Add round score to total score
+      scores[activePlayer] += roundScore;
+      document.querySelector('#score-'+activePlayer).textContent = scores[activePlayer];
+      //2. clear current score and change player
+      //3. check if player won the game
+      if(scores[activePlayer]>=20)
+      {
+        document.querySelector('#name-'+activePlayer).textContent = 'WINNER!';
+        document.querySelector('.dice').style.display = 'none';
+        document.querySelector('.player-'+activePlayer+'-panel').classList.add('winner');
+        document.querySelector('.player-'+activePlayer+'-panel').classList.remove('active');
+        stateVar = 0;
+      }
+      else
+        clearAndChange();
+    }
+})
+
+document.querySelector('.btn-new').addEventListener('click', () => {
+  resetVars();
+  clearCurrentScore();
+  clearGlobalScore();
+  document.querySelector('.player-0-panel').classList.remove('winner');
+  document.querySelector('.player-1-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('active');
+  document.querySelector('.player-0-panel').classList.remove('active');
+  document.querySelector('.player-0-panel').classList.add('active');
+})
